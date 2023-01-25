4530fe3b835957f7096080e9e91302f939147e86
Peter Elias El-Assal <pete443a@elev.tec.dk>
2023-01-25 17:37:55 +0100

    Final Product....

--- Menu.cs
+++ Menu.cs
@@ -1,8 +1,11 @@
-﻿namespace H1W2D3ToDoOrNotToDo
+﻿using System.Collections.Generic;
+
+namespace H1W2D3ToDoOrNotToDo
 {
     internal class Menu
     {
         List<ToDoItem> todoList = new();
+
 
         //Constructor that runs when object is instantiated (with new keyword).
         public Menu()
@@ -15,7 +18,7 @@
         //Start menu
         public void MainMenu()
         {
-            Console.WriteLine("Main Menu\n\n(1) Add new\n(2) Show List");
+            Console.WriteLine("Main Menu\n\n(1) Add new\n(2) Show List \n(3) Update \n(4) Delete");
 
             var v = Console.ReadKey(true);
             switch (v.KeyChar)
@@ -26,6 +29,12 @@
                 case '2':
                     ShowList();
                     break;
+                case '3':
+                    Update();
+                    break;
+                case '4':
+                    Delete();
+                    break;
                 default:
                     break;
             }
@@ -34,10 +43,12 @@
         //Show TodoList shows TodoItems
         void ShowList()
         {
-            foreach (ToDoItem? item in todoList)
+
+            foreach (ToDoItem item in todoList)
             {
                 ShowItem(item);
             }
+
         }
 
         //Create Item
@@ -64,18 +75,44 @@
         //Read TodoItem
         private void ShowItem(ToDoItem toDoItem)
         {
-            Console.WriteLine($"What: {toDoItem.Todo} When: {toDoItem.Deadline} isDone: {toDoItem.IsDone}");
+           
+                Console.WriteLine($"{todoList.IndexOf(toDoItem)}. What: {toDoItem.Todo} When: {toDoItem.Deadline} isDone: {toDoItem.IsDone}");
+    
+
             //Console.WriteLine("What: {0} When: {1} isDone: {2}", toDoItem.Todo, toDoItem.Deadline, toDoItem.IsDone);
             //Console.WriteLine("What: " + toDoItem.Todo + "When: " + toDoItem.Deadline + "isDone: " + toDoItem.IsDone);
         }
 
 
         //Update Item
+        public void Update()
+        {
+            
 
+            Console.WriteLine("Update");
+            ShowList();
+            Console.Write("Enter task number to Update: ");
+            int tasknum = int.Parse(Console.ReadLine());
+            todoList.RemoveAt(tasknum);
+            Console.Write("New Task");
+            Console.ReadLine();
+            CreateItem();
+
+        }
 
         //Delete Item
 
-
+        public void Delete()
+        {
+            Console.WriteLine("Delete");
+            ShowList();
+            Console.Write("Enter task number to delete: ");
+            int taskNum = Convert.ToInt32(Console.ReadLine());
+            todoList.RemoveAt(taskNum);
+          
+            Console.WriteLine("Task deleted!");
+            Console.ReadLine();
+        }
 
 
     }
