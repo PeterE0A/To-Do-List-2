a7842aca0547ef555989da4a399a572bbb6c0089
Henrik Vincents Poulsen <hpo@tec.dk>
2023-01-25 09:55:18 +0100

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,26 @@
+﻿namespace H1W2D3ToDoOrNotToDo
+{
+    internal class Menu
+    {
+        //Start menu
+
+
+        //Show TodoList
+
+
+        //Create Item
+
+
+        //Read TodoItem
+
+
+        //Update Item
+
+
+        //Delete Item
+
+
+
+
+    }
+}
---
+++ ToDoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace H1W2D3ToDoOrNotToDo
+{
+    /*
+ * To do item
+ *  Date Created
+ *  Date Deadline
+ *  Do / Done
+ *  Favorite
+ *  What to do Text
+ *  Repeat
+ *  
+ *  Create/Read/Update/Delete
+ * */
+    internal class ToDoItem
+    {
+        public DateTime Created { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsFavorite { get; set; }
+        public string? Todo { get; set; }
+        public long Repeat { get; set; } //If repeat > 0 then repeat is on
+    }
+}
